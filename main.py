ddedeb024423c4ad808ce3dc22ada6269f62616d
Braulio Carrion Corveira <45177553+Cinderguy23@users.noreply.github.com>
2021-04-17 16:28:54 -0400

    Fully finished done implementing preprocessing of data

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 # !pip install yfinance
 # !pip install --upgrade tensorflow
 
-print('test')
 import numpy as np
 import pandas as pd
 #from yahoofinancials import YahooFinancial
 import yfinance as yf
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
 # Get Data
 amd_stock = yf.Ticker('AMD')
@@ -27,38 +27,48 @@ tempList = list(range(0,limitOfDelete))
 amd_historyAll = amd_historyAll.drop(amd_historyAll.index[tempList])
 amd_historyAll = amd_historyAll.reset_index()
 amd_historyAll.pop('index')
+amd_historyAll.pop('Dividends')
+amd_historyAll.pop('Stock Splits')
+
+# Creating black variables
+xtrainingAll = pd.DataFrame(columns=['Open', 'High', 'Low','Close', 'Volume'])
+xtestingAll = pd.DataFrame(columns=['Open', 'High', 'Low','Close', 'Volume'])
+
+ytrainingAll = pd.DataFrame(columns=['Open', 'High', 'Low','Close', 'Volume'])
+ytestingAll = pd.DataFrame(columns=['Open', 'High', 'Low','Close', 'Volume'])
 
 # Splitting Data by 50 history points
-trainingAll = pd.DataFrame(columns=['Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'])
-validationAll = pd.DataFrame(columns=['Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'])
-testingAll = pd.DataFrame(columns=['Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'])
+for x in range(50, amd_historyAll.shape[0]):
+    if x%50 == 0:
+      xtrainingAll = xtrainingAll.append(amd_historyAll.loc[x-50:x])
+      xtestingAll = xtestingAll.append(amd_historyAll.iloc[x:x+1])
+    else:      
+      ytrainingAll = ytrainingAll.append(amd_historyAll.loc[x-50:x])
+      ytestingAll = ytestingAll.append(amd_historyAll.iloc[x:x+1])
 
-for x in range(int(rounded/50)):
-    trainingAll = trainingAll.append(amd_historyAll.loc[x*50:x*50+35])
-    validationAll = validationAll.append(amd_historyAll.iloc[x*50+35:x*50+41])
-    testingAll = testingAll.append(amd_historyAll.iloc[x*50+40:x*50+50])
-    
+xHistory = [xtrainingAll,xtestingAll]
+yHistory = [ytrainingAll,ytestingAll]
 
-#trainingAll = amd_historyAll[:int(len(amd_historyAll)*0.7)]
-#validationAll = amd_historyAll[int(len(amd_historyAll)*0.7):-int(len(amd_historyAll)*0.2)] 
-#testingAll = amd_historyAll[-int(len(amd_historyAll)*0.2):] 
+# Deleting the useless columns
+for x in xHistory:
+  x.pop('Close')
 
-# Normalize Data
-scaler = MinMaxScaler()
-trainingAll = scaler.fit_transform(trainingAll)
-validationAll = scaler.transform(validationAll)
-testingAll = scaler.transform(testingAll)
+for y in yHistory:
+  y.pop('Open')
+  y.pop('High')
+  y.pop('Low')
+  y.pop('Volume')
 
-# Print Statements
-testingAll,validationAll,testingAll = np.array(testingAll),np.array(validationAll),np.array(testingAll)
-print("--------------------Train------------------------")
-print(trainingAll)
-print("--------------------Val------------------------")
-print(validationAll)
-print("--------------------Test------------------------")
-print(testingAll)
+# Convert DataFrame to numpy array
+xtrainingAll1,xtestingAll1 = xtrainingAll.to_numpy(),xtestingAll.to_numpy()
+ytrainingAll1,ytestingAll1 = ytrainingAll.to_numpy(),ytestingAll.to_numpy()
 
-print("--------------------OG------------------------")
-print(amd_historyAll)
-print("--------------------OG------------------------")
-print(amd_historyAll.iloc[0:4])
+# Standarizing Data
+std = StandardScaler()
+xtrainingAll2 = std.fit_transform(xtrainingAll1)
+xtestingAll2 = std.transform(xtestingAll1)
+
+# Normalize, but wont change distribution and weights
+scaler = MinMaxScaler(feature_range=(0,1))
+xtrainingAll3 = scaler.fit_transform(xtrainingAll2)
+xtestingAll3 = scaler.transform(xtestingAll2)
