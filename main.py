b820357d99a1ca90f72027ea522f46fbcc7b090b
Braulio Carrion Corveira <45177553+Cinderguy23@users.noreply.github.com>
2021-04-24 14:26:05 -0400

    Finally got it working

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import os
 
 # Get Data
 amd_stock = yf.Ticker('AMD')
-amd_historyAll = amd_stock.history(period="3y").reset_index()
+amd_historyAll = amd_stock.history(period="5y").reset_index()
 
 # Delete date since numbers are just fine
 amd_historyAll.pop('Date')
@@ -43,49 +43,46 @@ xtestingAll = pd.DataFrame(columns=['Open', 'High', 'Low','Close', 'Volume'])
 ytrainingAll = pd.DataFrame(columns=['Open', 'High', 'Low','Close', 'Volume'])
 ytestingAll = pd.DataFrame(columns=['Open', 'High', 'Low','Close', 'Volume'])
 
-multiplier = 0
+
 # Splitting Data by 50 history points
 for x in range(amd_historyAll.shape[0]):
-    if x >= multiplier * 250 and x < multiplier * 250 + 50:
+    if x >= 1000:
       xtestingAll = xtestingAll.append(amd_historyAll.iloc[x])
       ytestingAll = xtestingAll.append(amd_historyAll.iloc[x])
+    elif x >= 950:
+      xtestingAll = xtestingAll.append(amd_historyAll.iloc[x])
+      ytestingAll = xtestingAll.append(amd_historyAll.iloc[x])
+      xtrainingAll = xtrainingAll.append(amd_historyAll.loc[x])
+      ytrainingAll = ytrainingAll.append(amd_historyAll.loc[x])
     else:      
       xtrainingAll = xtrainingAll.append(amd_historyAll.loc[x])
       ytrainingAll = ytrainingAll.append(amd_historyAll.loc[x])
-
-    if x == 250 * multiplier + 49:
-      multiplier+=1
-
-xtrainingAll1 = xtrainingAll.copy()
-xtestingAll1 = xtestingAll.copy()
-ytrainingAll1 = ytrainingAll.copy()
-ytestingAll1 = ytestingAll.copy()
 
 
 xHistory = [xtrainingAll,xtestingAll]
 yHistory = [ytrainingAll,ytestingAll]
 
 # Deleting the useless columns
-for x in xHistory:
-   x.pop('Close')
+# for x in xHistory:
+#    x.pop('Close')
 
-for y in yHistory:
-  y.pop('Open')
-  y.pop('High')
-  y.pop('Low')
-  y.pop('Volume')
+# for y in yHistory:
+#   y.pop('Open')
+#   y.pop('High')
+#   y.pop('Low')
+#   y.pop('Volume')
 
 # Convert DataFrame to numpy array
-xtrainingAll2,xtestingAll2 = np.array(xtrainingAll1),np.array(xtestingAll1)
-ytrainingAll2,ytestingAll2 = np.array(ytrainingAll1),np.array(ytestingAll1)
+xtrainingAll2,xtestingAll2 = np.array(xtrainingAll),np.array(xtestingAll)
+ytrainingAll2,ytestingAll2 = np.array(ytrainingAll),np.array(ytestingAll)
 
 # Standarizing Data
-std = StandardScaler()
-xtrainingAll3 = std.fit_transform(xtrainingAll2)
-xtestingAll3 = std.transform(xtestingAll2)
+# std = StandardScaler()
+# xtrainingAll3 = std.fit_transform(xtrainingAll2)
+# xtestingAll3 = std.transform(xtestingAll2)
 
 # Normalize, but wont change distribution and weights
-scaler = MinMaxScaler(feature_range=(-1,1))
+scaler = MinMaxScaler()#feature_range=(-1,1))
 xtrainingAll4 = scaler.fit_transform(xtrainingAll3)
 xtestingAll4 = scaler.transform(xtestingAll3)
 
@@ -117,31 +114,31 @@ else:
   # ? So I want to try starting the first layer with double the amount of neurons as the input size however have 50% dropout
   model = Sequential()
   # Input Layer
-  model.add(LSTM(units = 50, input_shape=(xtrainingAllFinal.shape[1],xtrainingAllFinal.shape[2]), return_sequences = True))
+  model.add(LSTM(units = 50, activation = 'relu', input_shape=(xtrainingAllFinal.shape[1],xtrainingAllFinal.shape[2]), return_sequences = True))
   # Hidden Layers
   model.add(Dropout(0.2))
-  model.add(LSTM(units = 70, return_sequences = True))
-  model.add(Dropout(0.2))
-  model.add(LSTM(units = 90, return_sequences = True))
-  model.add(Dropout(0.35))
-  model.add(LSTM(units = 120))
+  model.add(LSTM(units = 60, activation = 'relu', return_sequences = True))
+  model.add(Dropout(0.3))
+  model.add(LSTM(units = 80, activation = 'relu', return_sequences = True))
+  model.add(Dropout(0.4))
+  model.add(LSTM(units = 120, activation = 'relu'))
   model.add(Dropout(0.5))
   # Output Layer
   model.add(Dense(units = 1))
 
   model.summary()
-  model.compile(optimizer = 'adam', loss = "mean_squared_error", metrics = 'mean_squared_error')
-  history = model.fit(xtrainingAllFinal,ytrainingAllFinal,epochs=10,batch_size = 32, verbose = 1)
+  model.compile(optimizer = 'adam', loss = "mean_squared_error")
+  history = model.fit(xtrainingAllFinal,ytrainingAllFinal,epochs=10,batch_size = 32)
 
   model.save('model1')
 
 # Test Model
 yPred = model.predict(xtestingAllFinal)
-print('Scaler: ',scaler.scale_)
-print('MinMax: ',std)
-print(scaler.scale_[0])
+# print('Scaler: ',scaler.scale_)
+print('MinMax: ',scaler)
+# print(scaler.scale_[0])
 
-scl = 1/std.scale_[0]  
+scl = 1/scaler.scale_[0]  
 
 yPred1,ytestingAllFinal1 = yPred*scl,ytestingAllFinal*scl
 
